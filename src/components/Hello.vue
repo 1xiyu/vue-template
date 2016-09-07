c52bf2f07087aea50145925ed1225986c8f4644e
1xiyu <xiyu1smile@163.com>
2016-09-07 16:01:04 +0800

    webpack study

---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return{
+      title:'this is a todo list'
+    }
+  }
+  //es6语法 (同上)
+  //data () {
+    //return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      //msg: 'Hello World!'
+    //}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="msg">
+		<p>{{ message }}</p>
+		<input v-model="message" name="">
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				message:'Hello Vue.js!'
+			}
+		}
+	}
+</script>
